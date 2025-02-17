f46f0ac5ace1677aa7a414e0d0d6303b4786f78c
Filemon Robert Stevenson Kessing <filemonrsk@gmail.com>
2025-02-17 18:19:58 +0700

    Add api methods

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific HTTP methods
+    allowedHeaders: 'Content-Type, Accept', // Allow specific headers
+  });
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
